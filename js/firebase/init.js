7871978a9bf4741f73f780d07c3b605b69ca4085
Sven Erling Thorsen <sven@oculos.no>
2018-09-04 16:44:05 +0200

    Add firebase.js and configure it

---
+++ js/firebase/init.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyCGV6qihT_1h0rqeJkEC4PPwEVSjkp9Xvg",
+    authDomain: "ocws-test1.firebaseapp.com",
+    databaseURL: "https://ocws-test1.firebaseio.com",
+    projectId: "ocws-test1",
+    storageBucket: "ocws-test1.appspot.com",
+    messagingSenderId: "784608912345"
+  };
+  firebase.initializeApp(config);
